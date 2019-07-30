06c09a1592f9379b89d5a5b3f318d1a847da4234
mansourlotfi <mansourlotfi@gmail.com>
2019-07-30 17:30:48 +0430

    cors problem exist

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ let lat;
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position=>{
 console.log(position);
+long = position.coords.longitude;
+lat = position.coords.latitude;
+
+const proxy = "https://cors-anywhere.herokuapp.com";
+const api = `${proxy}https://api.darksky.net/forecast/008c945d1b6e1255fa9c410ab99bb210/${lat},${long}`;
+fetch (api)
+.then (response=>{
+    return response.json();
+})
+.then (data =>{
+    console.log(data);
+});
     });
+
 }
 });
